002a5d423993288facd62c19aa6b4c5f6eb9c0ec
Dayane Brito <dayane.brito@aguasdorio.com.br>
2024-05-27 17:18:37 -0300

    ajuste env

--- gamenv/Scripts/activate.bat
+++ gamenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\55219\OneDrive\Documentos\Python Scripts\game\game
+set VIRTUAL_ENV=C:\Users\dayane.brito\Documentos\PythonScripts\pygames\gamenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(game) %PROMPT%
+set PROMPT=(gamenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(game) 
+set VIRTUAL_ENV_PROMPT=(gamenv) 
 
 :END
 if defined _OLD_CODEPAGE (
